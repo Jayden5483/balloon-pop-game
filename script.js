8b66b164ad90e520ff62002e1f9bdd62230210a5
Jayden5483 <Khoaitayroberts@gmail.com>
2026-07-12 12:05:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,40 @@ $("#start").click(function() {
   $("#score").text(score);
   $("#gameArea").empty();
 
-  createBalloon();
+  for (let i = 0; i < 8; i++) {
+    createBalloon();
+  }
 });
+
+
+
+
+function createBalloon(balloon.css({
+  background: randomColor,
+  left: Math.random() * 830 + "px"
+});
+) {
+  let colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink"];
+
+  let randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+  let balloon = $("<div></div>");
+
+  balloon.addClass("balloon");
+
+  balloon.css({
+    background: randomColor
+  } balloon.click(function() {
+  $(this).stop();
+  $(this).fadeOut();
+});
+);
+
+  $("#gameArea").append balloon.animate({
+  bottom: "600px"
+}, 4000);
+(balloon);
+}
 
 function createBalloon() {
   let balloon = $("<div></div>");
