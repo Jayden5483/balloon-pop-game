9205e16177c9d7710eb032b2b4a71236afd0d8e2
Jayden5483 <Khoaitayroberts@gmail.com>
2026-07-12 11:47:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let score = 0;
 
+$("#start").click(function() {
+  score = 0;
+  $("#score").text(score);
+  $("#gameArea").empty();
+
+  createBalloon();
+});
+
+function createBalloon() {
+  let balloon = $("<div></div>");
+
+  balloon.addClass("balloon");
+
+  $("#gameArea").append(balloon);
+}
